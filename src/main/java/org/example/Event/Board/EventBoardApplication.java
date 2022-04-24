043d225d5ee8b9bff9b884e87ce57264ae673332
Alexander Feng <alexanderfzz.haha@gmail.com>
2022-04-24 12:11:27 -0400

    spring code combine test

---
+++ src/main/java/org/example/Event/Board/EventBoardApplication.java
@@ -0,0 +1,13 @@
+package org.example.Event.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBoardApplication.class, args);
+	}
+
+}
