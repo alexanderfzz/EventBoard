5fffc00099ec760447001d6aab90d94783d7f82e
Alexander Feng <alexanderfzz.haha@gmail.com>
2022-04-25 11:54:29 -0400

    renamed some file structures

--- src/main/java/Spring/EventBoardApplication.java
+++ src/main/java/Spring/EventBoardApplication.java
@@ -1,4 +1,4 @@
-package org.example.Event.Board;
+package Spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/Spring/EventBoardApplicationTests.java
+++ src/test/java/Spring/EventBoardApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.Event.Board;
+package Spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
